6c2acb5f253b2c6dd1109bbd0e53c6929036bd72
sisafa-collab <hnbra.execucaofinanceira@gmail.com>
2026-05-26 14:16:35 +0000

    scatterplot18

--- app.py
+++ app.py
@@ -4436,7 +4436,7 @@ Cordialmente,
                     # =================================================================
                     # 🎯 RADAR 3D EXECUTIVO: CICLO STATUS 6 -> 7
                     # =================================================================
-                    st.markdown("<br><br>#### 🎯 Radar de Risco Financeiro: Status 6 (Fiscalização)", unsafe_allow_html=True)
+                    st.markdown("<br><br>🎯 Radar dos fiscais de contrato: gráfico de dispersão 📈", unsafe_allow_html=True)
 
                     # 1. Inicialização segura
                     df_radar = pd.DataFrame() 
@@ -4472,7 +4472,7 @@ Cordialmente,
                         df_radar['Dias_Fisc'] = (df_radar['Retorno_Exec'] - df_radar['Entrada_Fisc']).dt.total_seconds() / 86400
                         df_radar['Valor_Num'] = df_radar['valor_liquido'].apply(limpar_valor)
                         
-                        ordem_sla = ['🟢 Ideal (≤20d)', '🟠 Atenção (21-30d)', '🔴 Crítico (>30d)']
+                        ordem_sla = ['Ideal (≤20d)', 'Atenção (21-30d)', 'Crítico (>30d)']
                         df_radar['SLA'] = pd.cut(df_radar['Dias_Fisc'], bins=[-1, 20, 30, 9999], labels=ordem_sla)
                         df_radar['Tamanho_Bolha'] = df_radar['Valor_Num'].apply(lambda x: x if x > 1000 else 1000)
                         df_radar['Entrada_Fisc_Str'] = df_radar['Entrada_Fisc'].dt.strftime('%d/%m/%Y')
