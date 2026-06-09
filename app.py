ab5ef5c5ac78238dd13dbd9855625a99adf13462
sisafa-collab <hnbra.execucaofinanceira@gmail.com>
2026-06-09 18:54:58 +0000

    relatorio-empresa-7

--- app.py
+++ app.py
@@ -773,7 +773,7 @@ def gerar_relatorio_ose_pdf(ose_nome, df_ose, volume_total, qtd_total, fig_pie):
     # --- 5. RODAPÉ ---
     pdf.set_y(220) 
     if os.path.exists("mapeamento-de-processo.png"):
-        pdf.image("mapeamento-de-processo.png", x=10, y=220, w=190, h=0)
+        pdf.image("mapeamento-de-processo.png", x=10, y=220, w=70, h=0)
     pdf.set_xy(10, 270)
     pdf.set_font("Arial", 'I', 14)
     msg = "Esperamos fortalecer a confiança mútua e a parceria com o Hospital Naval de Brasília. Somos gratos pelo apoio e pela distinta cooperação.\n\nHospital Naval de Brasília - A Saúde Naval no Planalto Central!"
